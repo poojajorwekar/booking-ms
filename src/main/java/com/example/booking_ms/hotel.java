dc3761fc03991f3e003bb288db4d073cccadc045
pooja <pooja.jorwekar811@gmail.com>
2025-07-28 17:24:43 +0530

    firstcode11

---
+++ src/main/java/com/example/booking_ms/hotel.java
@@ -0,0 +1,12 @@
+package com.example.booking_ms;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class hotel {
+    @GetMapping("/hotel")
+    public String getData() {
+        return "Please  book hotel ticket with 30% discount";
+}
+}
